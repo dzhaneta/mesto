a878b7cdbc861b5aafc9f669ce5b5a4a64796e6f
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2022-06-13 20:55:43 +0300

    test commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,44 +3,52 @@ let profileAbout = document.querySelector('.profile__bio');
 
 let editProfilePopup = document.querySelector('.popup_type_edit-profile');
 let editProfileButton = document.querySelector('.profile__edit-button');
-let closePopupButton = editProfilePopup.querySelector('.popup__close-button');
+let closeEditProfilePopupButton = editProfilePopup.querySelector('.popup__close-button');
 
 let formElement = document.querySelector('.form_type_edit-profile');
 let nameInput = document.querySelector('.form__input_type_username');
 let jobInput = document.querySelector('.form__input_type_userabout');
 
-function togglePopup() {
-  editProfilePopup.classList.toggle('popup_opened');
+let likePhotoButton = document.querySelectorAll('.photo-grid__like-button');
+
+function togglePopup(targetPopup) {
+  targetPopup.classList.toggle('popup_opened');
 }
 
 function fetchAndOpenProfile() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileAbout.textContent;
-  togglePopup();
+  togglePopup(editProfilePopup);
 }
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                              // Так мы можем определить свою логику отправки.
-                                              // О том, как это делать, расскажем позже.
-
-  // Получите значение полей jobInput и nameInput из свойства value
-  // Выберите элементы, куда должны быть вставлены значения полей
-  // Вставьте новые значения с помощью textContent
+  evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = jobInput.value;
-  togglePopup();
+  togglePopup(editProfilePopup);
 }
 
 
 editProfileButton.addEventListener('click', fetchAndOpenProfile);
 
+closeEditProfilePopupButton.addEventListener('click', togglePopup(editProfilePopup));
 
-closePopupButton.addEventListener('click', togglePopup);
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+likePhotoButton.forEach(likePhotoButton => {
+    likePhotoButton.addEventListener('click', function(evt) {
+      likePhotoButton.classList.toggle('photo-grid__like-button_active')
+    })
+  }
+);
+
+
+
+let addCardPopup = document.querySelector('.popup_type_add-card');
+let addCardButton = document.querySelector('.profile__add-button');
+let closeAddCardPopupButton = addCardPopup.querySelector('.popup__close-button');
+
 
